e7e82bdd0da70f78b2f5bed45da7335f4ff71aa7
najwa900 <zaneti951@gmail.com>
2026-05-25 11:56:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def load_data():
     path_primer = os.path.join(DATA_DIR, "Dataset_Terstruktur_Primer_NOPI.csv")
     path_evaluasi = os.path.join(DATA_DIR, "evaluasi_3_model.csv")
     path_detail = os.path.join(DATA_DIR, "detail_akurasi_semua_model.csv")
-    path_clean = os.path.join(DATA_DIR, "dataset_ocr_clear_final.csv")
+    path_clean = os.path.join(DATA_DIR, "dataset_ocr_clean_final.csv")
     path_all = os.path.join(DATA_DIR, "all_images_metadata.csv")
 
     # cek file wajib
